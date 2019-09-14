2f12c906c7827b5f0f2d97b8a2f3cf205b14286f
Michael Meisel <mrmeisel@gmail.com>
2019-09-14 14:06:36 -0700

    Draw venues on the map with an ugly green dot

--- scripts/map.js
+++ scripts/map.js
@@ -1,15 +1,21 @@
 $(document).ready(() => {
   const svg = d3.select('#map').append('svg');
+
+  // Add separate groups for the base map and the venue points to ensure the venues render on top
+  // of the map. Order matters!
+  const baseMapGroup = svg.append('g');
+  const venueGroup = svg.append('g');
+
   // California Albers projection, see:
   // https://medium.com/@mbostock/command-line-cartography-part-1-897aa8f8ca2c
   const projection = d3.geoConicEqualArea().parallels([34, 40.5]).rotate([120, 0]);
   const geoPath = d3.geoPath().projection(projection);
-  let sfMesh
+  let sfMesh;
 
   d3.json('../sf/streets.json').then(sf => {
     sfMesh = topojson.mesh(sf);
 
-    svg.append('path');
+    baseMapGroup.append('path');
     resizeMap();
     $(window).resize(resizeMap);
   });
@@ -17,9 +23,17 @@ $(document).ready(() => {
   function resizeMap() {
     const mapContainer = $('#map');
 
-    projection.fitSize([mapContainer.innerWidth(), mapContainer.innerHeight()], sfMesh);
+    projection.fitSize([mapContainer.innerWidth(), mapContainer.innerHeight() * 1.5], sfMesh);
 
     svg.select('path')
       .attr('d', geoPath(sfMesh));
+
+    const venuePoints = venueGroup.selectAll('circle').data(venues, venue => venue.venueId);
+
+    venuePoints.join('circle')
+      .attr('class', 'venue')
+      .attr('r', 10)
+      .attr('cx', venue => projection(venue.location.slice().reverse())[0])
+      .attr('cy', venue => projection(venue.location.slice().reverse())[1]);
   }
 });
