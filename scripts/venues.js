25d046e6007620803cbf763bb3bec9e5adb78d3e
Whitney <whits129@gmail.com>
2019-09-14 12:59:52 -0700

    added first venue

---
+++ scripts/venues.js
@@ -0,0 +1,8 @@
+let venues = [
+  { name: "Swedish American Music Hall/Cafe du Nord", venueId: "swedish", location: [37.766768, -122.430501]}
+]
+
+let shows = [
+{artist: "Victor Internet", date: "Sep/19/2019 7:30 PM", blurb: "Cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+]
