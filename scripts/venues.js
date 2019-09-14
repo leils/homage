f6b9df46d4aeb64f57298a7d53be69dad0823b93
Whitney <whits129@gmail.com>
2019-09-14 13:51:05 -0700

    more venues!

--- scripts/venues.js
+++ scripts/venues.js
@@ -1,8 +1,36 @@
 let venues = [
-  { name: "Swedish American Music Hall/Cafe du Nord", venueId: "swedish", location: [37.766768, -122.430501]}
+  { name: "Swedish American Music Hall/Cafe du Nord", venueId: "swedish", location: [37.766768, -122.430501] },
+  { name: "The Fillmore", venueId: "fillmore", location: [37.783934, -122.433073] },
+  { name: "Great American Music Hall", venueId: "gamh", location: [37.784957, -122.418871] },
+  { name: "The Chapel", venueId: "chapel", location: [37.760527, -122.421302] },
+  { name: "Hotel Utah", venueId: "utah", location: [37.779438, -122.398062] },
+  { name: "Rickshaw Stop", venueId: "rickshaw", location: [37.776137, -122.420503] },
+  { name: "Slim's", venueId: "slims", location: [37.771592, -122.413371] },
+  { name: "Bimbo's", venueId: "bimbos", location: [37.803755, -122.415592] },
+  { name: "Thee Parkside", venueId: "parkside", location: [37.765264, -122.399916] },
+  { name: "Bottom of the Hill", venueId: "bottom", location: [37.765084, -122.396459] },
+  { name: "Independent", venueId: "independent", location: [37.775614, -122.437706] },
+  { name: "Neck of the Woods", venueId: "neck", location: [37.783139, -122.463752] },
+  { name: "The Knockout", venueId: "knockout", location: [37.745185, -122.419981] },
+
+
 ]
 
-let shows = [
-{artist: "Victor Internet", date: "Sep/19/2019 7:30 PM", blurb: "Cool name", venueId: "swedish"}
+let videos = [
+{artist: "The Marias", video: "https://www.youtube.com/watch?v=YzKM5g_FwYU",  venueId: "swedish"}
 {artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+{artist: "Boogarins", date: "Sep/21/2019 7:30 PM", blurb: "Also cool name", venueId: "swedish"}
+
 ]
